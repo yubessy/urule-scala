babac4fe8bcd059574bc46618c8a36299e25207e
yubessy <yubessy0@gmail.com>
2017-01-06 17:13:55 +0900

    Add list type YAML test

--- src/test/scala/com/github/yubessy/urule/test/RuleSpec.scala
+++ src/test/scala/com/github/yubessy/urule/test/RuleSpec.scala
@@ -6,7 +6,7 @@ import com.github.yubessy.urule.types.RuleMap
 import com.netaporter.uri.Uri.parse
 
 class RuleSpec extends BaseSpec {
-  describe("integration") {
+  describe("map type") {
     val ruleMap = YamlLoader.load("map_example").asInstanceOf[RuleMap]
     val rule = Rule(ruleMap)
 
@@ -23,6 +23,26 @@ class RuleSpec extends BaseSpec {
     it("should match no rule") {
       val uri = parse("https://example.com/baz/")
       rule.extract(uri) should equal(None)
+    }
+  }
+
+  describe("list type") {
+    val ruleMap = YamlLoader.load("list_example").asInstanceOf[Seq[RuleMap]]
+    val rule = Rule(ruleMap)
+
+    it("should match first rule") {
+      val uri = parse("https://example.com/")
+      rule.extract(uri) should equal(Some("zero"))
+    }
+
+    it("should match second rule") {
+      val uri = parse("https://subdomain.example.com/")
+      rule.extract(uri) should equal(Some("one"))
+    }
+
+    it("should match third rule") {
+      val uri = parse("https://subsub.domain.example.com/")
+      rule.extract(uri) should equal(Some("two"))
     }
   }
 }
