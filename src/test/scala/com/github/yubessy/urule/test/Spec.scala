266e1f389982a51b50703e774041a349f60d5ef9
yubessy <yubessy0@gmail.com>
2017-01-13 23:15:59 +0900

    Refactor

--- src/test/scala/com/github/yubessy/urule/test/Spec.scala
+++ src/test/scala/com/github/yubessy/urule/test/Spec.scala
@@ -5,27 +5,31 @@ import com.github.yubessy.urule.test.helper.YamlLoader
 import org.scalatest._
 
 class Spec extends FunSpec with Matchers {
+  def getRule(raw: Any): Rule =
+    raw match {
+      case e: RawElem => Rule(e)
+      case s: Seq[RawElem] => Rule(s)
+    }
+
+  def getExpected(raw: RawElem): Option[Result] =
+    if (raw.keys.exists(Extractor.keys.contains)) {
+      Some(Result(
+        category = raw.get("category").map(_.asInstanceOf[String]),
+        attrs = raw.get("attrs").map(_.asInstanceOf[Map[String, String]]).getOrElse(Map.empty)
+      ))
+    } else {
+      None
+    }
+
   describe("test cases") {
     val cases = YamlLoader.load("cases").asInstanceOf[Seq[Map[String, _]]]
     cases.foreach(m => {
-      val rule = m("rule") match {
-        case e: RawElem => Rule(e)
-        case s: Seq[RawElem] => Rule(s)
-      }
-
       val caption = m("caption").asInstanceOf[String]
+      val rule = getRule(m("rule"))
       describe(caption) {
-        m("cases").asInstanceOf[Seq[Map[String, _]]].foreach(c => {
-          val expected = if (c.keys.exists(k => Seq("category", "attrs").contains(k))) {
-            Some(Result(
-              category = c.get("category").asInstanceOf[Option[String]],
-              attrs = c.get("attrs").map(_.asInstanceOf[Map[String, String]]).getOrElse(Map.empty)
-            ))
-          } else {
-            None
-          }
-
+        m("cases").asInstanceOf[Seq[RawElem]].foreach(c => {
           val url = c("url").asInstanceOf[String]
+          val expected = getExpected(c)
           it(url) { rule.applyTo(url) should equal(expected) }
         })
       }
